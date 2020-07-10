ed3b95c395e6b15317ca36aebcebf723f2ad6be9
Jonatas Felipe <jonatasfelippe@gmail.com>
2020-07-09 22:20:22 -0300

    Tratamento de erro finalizado

--- api_2_7_dias.py
+++ api_2_7_dias.py
@@ -2,51 +2,52 @@ from datetime import datetime
 from api_clima_tempo import *
 
 one_call_api = 'eda5c12594a03dc52dc2018191872092'
-key = 'e81f069dd11a3d0de4e29facc48edb79'
+key = 'de5d763fc253d3738b81b7feceff35d0'
 
 API_CALL = ('https://api.openweathermap.org/data/2.5/onecall?'
-            'lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=e81f069dd11a3d0de4e29facc48edb79')
+            'lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=de5d763fc253d3738b81b7feceff35d0')
 
 def long_lat():
+    city = input('Digite a city: ')
+    domain = ('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=de5d763fc253d3738b81b7feceff35d0')
     try:
-        city = input('Digite a city: ')
-        domain = ('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e81f069dd11a3d0de4e29facc48edb79')
         r = requests.get(domain)
         parse_json = json.loads(r.text)
         lon = parse_json['coord']['lat']
         lat = parse_json['coord']['lon']
         return (lon, lat)
     except Exception as e:
+        print('{} fomarto inválido, digite uma cidade válida'.format(city))
         print(e)
 
 def date_7_days():
-    lon_lat = long_lat()
-    lati = lon_lat[0]
-    longi = lon_lat[1]
-    lat = str(lati)
-    lon = str(longi)
-    api_call = ('https://api.openweathermap.org/data/2.5/onecall?'
-                'lat='+lat+'&lon='+lon+'&exclude=hourly&appid=e81f069dd11a3d0de4e29facc48edb79')
-    r = requests.get(api_call)
-    parse_json = json.loads(r.text)
-    #prev_2_days = parse_json
-    prev_7_days = parse_json
-
-    hoje = prev_7_days['daily'][0]
-    segunda = prev_7_days['daily'][1]
-    terca = prev_7_days['daily'][2]
-    quarta = prev_7_days['daily'][3]
-    quinta = prev_7_days['daily'][4]
-    sexta = prev_7_days['daily'][5]
-    sabado = prev_7_days['daily'][6]
-    domingo = prev_7_days['daily'][7]
-    dias = [hoje, segunda, terca, quarta, quinta, sexta, sabado, domingo]
-    DIAS = []
-    for dia in dias:
-        data = dia['dt']
-        DIAS.append(data)
-    return(DIAS)
-
+    try:
+        lon_lat = long_lat()
+        lati = lon_lat[0]
+        longi = lon_lat[1]
+        lat = str(lati)
+        lon = str(longi)
+        api_call = ('https://api.openweathermap.org/data/2.5/onecall?'
+                    'lat=' + lat + '&lon=' + lon + '&exclude=hourly&appid=de5d763fc253d3738b81b7feceff35d0')
+        r = requests.get(api_call)
+        parse_json = json.loads(r.text)
+        prev_7_days = parse_json
+        hoje = prev_7_days['daily'][0]
+        segunda = prev_7_days['daily'][1]
+        terca = prev_7_days['daily'][2]
+        quarta = prev_7_days['daily'][3]
+        quinta = prev_7_days['daily'][4]
+        sexta = prev_7_days['daily'][5]
+        sabado = prev_7_days['daily'][6]
+        domingo = prev_7_days['daily'][7]
+        dias = [hoje, segunda, terca, quarta, quinta, sexta, sabado, domingo]
+        DIAS = []
+        for dia in dias:
+            data = dia['dt']
+            DIAS.append(data)
+        return(DIAS)
+    except Exception as e:
+        print(e)
 
 def data(ver_forecast):
     data = []
@@ -57,49 +58,50 @@ def data(ver_forecast):
         data.append(dat)
     return(data)
 
-
 def ver_temperature(dia):
-    lon_lat = long_lat()
-    lati = lon_lat[0]
-    longi = lon_lat[1]
-    lat = str(lati)
-    lon = str(longi)
-    api_call = ('https://api.openweathermap.org/data/2.5/onecall?'
-                'lat=' + lat + '&lon=' + lon + '&exclude=hourly&appid=your-token-key')
-    r = requests.get(api_call)
-    parse_json = json.loads(r.text)
-    prev_7_days = parse_json
+    try:
+        lon_lat = long_lat()
+        lati = lon_lat[0]
+        longi = lon_lat[1]
+        lat = str(lati)
+        lon = str(longi)
+        api_call = ('https://api.openweathermap.org/data/2.5/onecall?'
+                    'lat=' + lat + '&lon=' + lon + '&exclude=hourly&appid=your-token-key')
+        r = requests.get(api_call)
+        parse_json = json.loads(r.text)
+        prev_7_days = parse_json
 
-    hoje = prev_7_days['daily'][dia]
-    data = hoje['dt']
-    temp = hoje['temp']
-    day = temp['day']
-    min = temp['min']
-    max = temp['max']
-    weather = hoje['weather']
-    main = weather[0]['main']
-    description = weather[0]['description']
-    temp_kelvin = [day, min, max]
-    temp_celsius = []
-    for i in temp_kelvin:
-        t = i
-        temp = (int(t) - 273.15)
-        r = round(temp)
-        temp_celsius.append(r)
-    ts = data
-    dat = (datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-    day = ('day {}ºC'.format(temp_celsius[0]))
-    min = ('min {}ºC'.format(temp_celsius[1]))
-    max = ('max {}ºC '.format(temp_celsius[2]))
-    print('-------------------------------------')
-    print('Data e hora {}'.format(dat))
-    print(day)
-    print(min)
-    print(max)
-    print('main {}'.format(main))
-    print('description {}'.format(description))
-    print('-------------------------------------')
-
+        hoje = prev_7_days['daily'][dia]
+        data = hoje['dt']
+        temp = hoje['temp']
+        day = temp['day']
+        min = temp['min']
+        max = temp['max']
+        weather = hoje['weather']
+        main = weather[0]['main']
+        description = weather[0]['description']
+        temp_kelvin = [day, min, max]
+        temp_celsius = []
+        for i in temp_kelvin:
+            t = i
+            temp = (int(t) - 273.15)
+            r = round(temp)
+            temp_celsius.append(r)
+        ts = data
+        dat = (datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+        day = ('day {}ºC'.format(temp_celsius[0]))
+        min = ('min {}ºC'.format(temp_celsius[1]))
+        max = ('max {}ºC '.format(temp_celsius[2]))
+        print('-------------------------------------')
+        print('Data e hora {}'.format(dat))
+        print(day)
+        print(min)
+        print(max)
+        print('main {}'.format(main))
+        print('description {}'.format(description))
+        print('-------------------------------------')
+    except Exception as e:
+        print(e)
 
 def mostrar_menu():
     print('#################################')
--- api_clima_tempo.py
+++ api_clima_tempo.py
@@ -1,42 +1,46 @@
 import requests
 import json
 
-key = 'your-token-key'
+key = 'de5d763fc253d3738b81b7feceff35d0'
 
 def weather_today():
     cidade = input('Digite a cidade: ')
-    DOMINIO = ('http://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=your-token-key')
-    r = requests.get(DOMINIO)
-    parse_json = json.loads(r.text)
+    DOMINIO = ('http://api.openweathermap.org/data/2.5/weather?q=' + cidade + '&appid=de5d763fc253d3738b81b7feceff35d0')
+    try:
+        r = requests.get(DOMINIO)
+        parse_json = json.loads(r.text)
 
-    temperatura = parse_json['main']['temp']
-    temperatura_min = parse_json['main']['temp_min']
-    temperatura_max = parse_json['main']['temp_max']
+        temperatura = parse_json['main']['temp']
+        temperatura_min = parse_json['main']['temp_min']
+        temperatura_max = parse_json['main']['temp_max']
 
-    tempo = parse_json['weather'][0]['main']
-    tempo_desc = parse_json['weather'][0]['description']
+        tempo = parse_json['weather'][0]['main']
+        tempo_desc = parse_json['weather'][0]['description']
 
-    TEMPERATURAS = [
-        temperatura,
-        temperatura_min,
-        temperatura_max,
-    ]
+        TEMPERATURAS = [
+            temperatura,
+            temperatura_min,
+            temperatura_max,
+        ]
 
-    VER_TEMPERATURAS = []
+        VER_TEMPERATURAS = []
 
-    for temperatura in TEMPERATURAS:
-        t = temperatura
-        temp = (int(t) - 273.15)
-        r = round(temp, 0)
-        VER_TEMPERATURAS.append(r)
+        for temperatura in TEMPERATURAS:
+            t = temperatura
+            temp = (int(t) - 273.15)
+            r = round(temp, 0)
+            VER_TEMPERATURAS.append(r)
 
-    consulta1 = VER_TEMPERATURAS[0]
-    consulta2 = VER_TEMPERATURAS[1]
-    consulta3 = VER_TEMPERATURAS[2]
-    print('------------------------------------------')
-    print('\nTemperatura agora {}ªC'.format(consulta1))
-    print('Temperatura minima {}ªC'.format(consulta2))
-    print('Temperatura Máxima {}ªC'.format(consulta3))
-    print('Main {}'.format(tempo))
-    print('description {}'.format(tempo_desc))
-    print('------------------------------------------')
+        consulta1 = VER_TEMPERATURAS[0]
+        consulta2 = VER_TEMPERATURAS[1]
+        consulta3 = VER_TEMPERATURAS[2]
+        print('------------------------------------------')
+        print('\nTemperatura agora {}ªC'.format(consulta1))
+        print('Temperatura minima {}ªC'.format(consulta2))
+        print('Temperatura Máxima {}ªC'.format(consulta3))
+        print('Main {}'.format(tempo))
+        print('description {}'.format(tempo_desc))
+        print('------------------------------------------')
+    except Exception as e:
+        print('{} invália, especifique um nome válido'.format(cidade))
+
